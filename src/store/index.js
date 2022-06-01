dabb1b137b8ee43420c4eabf1a10c31c14d4080e
Thien Trinh <thien.trinh@hcl.com>
2022-06-01 12:24:54 +0700

    Create and apply store

---
+++ src/store/index.js
@@ -0,0 +1,20 @@
+import { legacy_createStore as createStore } from 'redux';
+
+const counterReducer = (state = { counter: 0 }, action) => {
+  switch (action.type) {
+    case 'increment':
+      return {
+        counter: state.counter + 1
+      };
+    case 'decrement':
+      return {
+        counter: state.counter - 1
+      };
+    default:
+      return state;
+  }
+};
+
+const store = createStore(counterReducer);
+
+export default store;
