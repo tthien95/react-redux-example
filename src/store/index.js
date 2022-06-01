9ca23777030c3712fc9e104e41157da0e89dd62e
Thien Trinh <thien.trinh@hcl.com>
2022-06-01 16:26:06 +0700

    Refactor Redux with Redux toolkit

--- src/store/index.js
+++ src/store/index.js
@@ -1,31 +1,30 @@
-import { legacy_createStore as createStore } from 'redux';
+import { createSlice, configureStore } from '@reduxjs/toolkit';
 
 const initialState = { counter: 0, showCounter: true };
 
-const counterReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case 'increment':
-      return {
-        counter: state.counter + 1,
-        showCounter: state.showCounter
-      };
-    case 'increase':
-      return {
-        counter: state.counter + action.amount,
-        showCounter: state.showCounter
-      };
-    case 'decrement':
-      return {
-        counter: state.counter - 1,
-        showCounter: state.showCounter
-      };
-    case 'toggle':
-      return { showCounter: !state.showCounter, counter: state.counter };
-    default:
-      return state;
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter += action.payload;
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter;
+    }
   }
-};
+});
 
-const store = createStore(counterReducer);
+const store = configureStore({
+  reducer: counterSlice.reducer
+});
+
+export const counterActions = counterSlice.actions;
 
 export default store;
