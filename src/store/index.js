6ed0d4d6de04c029676a8462252ffe536d731303
Thien Trinh <thien.trinh@hcl.com>
2022-06-01 15:20:06 +0700

    Handle multiple states with Redux

--- src/store/index.js
+++ src/store/index.js
@@ -1,19 +1,26 @@
 import { legacy_createStore as createStore } from 'redux';
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const initialState = { counter: 0, showCounter: true };
+
+const counterReducer = (state = initialState, action) => {
   switch (action.type) {
     case 'increment':
       return {
-        counter: state.counter + 1
+        counter: state.counter + 1,
+        showCounter: state.showCounter
       };
     case 'increase':
       return {
-        counter: state.counter + action.amount
+        counter: state.counter + action.amount,
+        showCounter: state.showCounter
       };
     case 'decrement':
       return {
-        counter: state.counter - 1
+        counter: state.counter - 1,
+        showCounter: state.showCounter
       };
+    case 'toggle':
+      return { showCounter: !state.showCounter, counter: state.counter };
     default:
       return state;
   }
